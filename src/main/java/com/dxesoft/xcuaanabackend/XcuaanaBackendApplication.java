0a2093c43c8ee18702a3a016594757db3e09f911
Ernesto Mendez <ernesto.mendez@ihg.com>
2019-04-16 19:43:36 -0500

    Initial commit

---
+++ src/main/java/com/dxesoft/xcuaanabackend/XcuaanaBackendApplication.java
@@ -0,0 +1,13 @@
+package com.dxesoft.xcuaanabackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XcuaanaBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XcuaanaBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dxesoft/xcuaanabackend/XcuaanaBackendApplicationTests.java
@@ -0,0 +1,16 @@
+package com.dxesoft.xcuaanabackend;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class XcuaanaBackendApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
